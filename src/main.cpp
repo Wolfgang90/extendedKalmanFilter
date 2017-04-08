a41740271b9e647c77f7db9d0278ae40ee8607ed
Wolfgang90 <eichler_wolfgang@msn.com>
2017-04-08 14:26:32 +0200

    Command line arguments implemented
    Implemented command line arguments, including check for appropriate number of inputs.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
-int main(){
+void check_arguments(int argc, char *argv[]){
+
+  // Initialize reply string if input is invalid
+  stringstream ss;
+  ss << "Please enter the following command line arguments: " << argv[0] << " path/to/input-file.txt output-file.txt";
+  string command_input = ss.str();
+
+  // Initialize tracker for argument validity
+  bool args_valid = false;
+
+  if (argc <= 1) {
+    cerr << command_input << endl;
+  } else if (argc == 2) {
+    cerr << "Please include an output file.\n" << command_input << endl;
+  } else if (argc == 3) {
+    args_valid = true;
+  } else if (argc > 3) {
+    cerr << "Too many arguments provided.\n" << command_input << endl;
+  }
+
+  if (!args_valid) {
+    exit(EXIT_FAILURE);
+  }
+
+}
+
+int main(int argc, char *argv[]){
   cout << "Program started successfully" << endl;
+  
+  check_arguments(argc, argv);
+
+  //Load data from input file
+
 
   return 0;
 }
