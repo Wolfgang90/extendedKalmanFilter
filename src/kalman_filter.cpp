65d2efe672f8fae3a1d09245ff8796be29ed91fb
Wolfgang90 <eichler_wolfgang@msn.com>
2017-04-10 16:34:05 +0200

    Simple Kalman-Filter update implemented

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -24,7 +24,19 @@ void KalmanFilter::Predict(){
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  //Update state by using Kalman Filter equations
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = Pht * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
